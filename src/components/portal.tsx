4b5c4047735081f33dfc87a5c75d8516a00fcf11
Ben Halverson <benhalverson33@gmail.com>
2022-09-24 02:32:16 -0700

    liniting

--- src/components/portal.tsx
+++ src/components/portal.tsx
@@ -2,36 +2,42 @@ import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 export const QuickPortal: React.FC<{
-    children: any;
-    className?: string;
-    element?: string;
-    style?: object;
-    renderIn?: HTMLElement;
-}> = ({ children, className, element = 'div', style, renderIn = document.body }) => {
-    const [container] = useState(() => {
-        const el = document.createElement(element);
-        const classNames = className?.split(' ').filter(item => item);
-        if (classNames && classNames.length > 0) {
-            classNames.forEach(portalClass => el.classList.add(portalClass));
-        }
-        if (style) {
-            const styleString = style
-                ? Object.keys(style).reduce((previous, current) => {
-                    //   return previous.concat(`${current}: ${style[current]}; `);
-                    return previous.concat(`${current}`)
-                  }, '')
-                : '';
-            el.setAttribute('style', styleString);
-        }
-        return el;
-    });
+  children: any;
+  className?: string;
+  element?: string;
+  style?: object;
+  renderIn?: HTMLElement;
+}> = ({
+  children,
+  className,
+  element = 'div',
+  style,
+  renderIn = document.body,
+}) => {
+  const [container] = useState(() => {
+    const el = document.createElement(element);
+    const classNames = className?.split(' ').filter(item => item);
+    if (classNames && classNames.length > 0) {
+      classNames.forEach(portalClass => el.classList.add(portalClass));
+    }
+    if (style) {
+      const styleString = style
+        ? Object.keys(style).reduce((previous, current) => {
+            //   return previous.concat(`${current}: ${style[current]}; `);
+            return previous.concat(`${current}`);
+          }, '')
+        : '';
+      el.setAttribute('style', styleString);
+    }
+    return el;
+  });
 
-    useEffect(() => {
-        renderIn.appendChild(container);
-        return () => {
-            renderIn.removeChild(container);
-        };
-    }, []);
+  useEffect(() => {
+    renderIn.appendChild(container);
+    return () => {
+      renderIn.removeChild(container);
+    };
+  }, []);
 
-    return createPortal(children, container);
+  return createPortal(children, container);
 };
